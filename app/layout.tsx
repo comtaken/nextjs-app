93140725f74d067371621e75b1ba02c84a3e331d
comtaken <kentaknkn@gmail.com>
2025-02-24 13:08:46 +0900

    共通レイアウト設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { collectRoutesUsingEdgeRuntime } from "next/dist/build/utils";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h4>共通ヘッダー</h4>
         {children}
+        <h4>共通フッター</h4>
       </body>
     </html>
   );
---
+++ app/pages/next/page.tsx
@@ -0,0 +1,13 @@
+// app/pages/next/page.tsx
+'use client';
+import { useEffect, useState } from 'react';
+
+export default function Next() {
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>next</h1>
+      <a href="/pages/top">back</a>
+    </div>
+  );
+}
